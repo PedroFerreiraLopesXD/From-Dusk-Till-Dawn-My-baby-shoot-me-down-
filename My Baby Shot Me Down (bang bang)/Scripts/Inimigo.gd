d8bf2ec2c54408a3a6e809f32ea7b6939790421d
PedroFerreiraLopesXD <rezelovers@gmail.com>
2026-06-16 11:36:46 -0300

    Fracasso

--- My Baby Shot Me Down (bang bang)/Scripts/Inimigo.gd
+++ My Baby Shot Me Down (bang bang)/Scripts/Inimigo.gd
@@ -2,17 +2,16 @@ extends CharacterBody3D
 
 @onready var jogador =  get_tree().get_first_node_in_group("Jogador")
 @onready var ray = $RayCast3D
+@onready var timer = $Timer
 
 var speed = 5.0
 var distancia_ataque = 15.0
 var cooldown = 1.0
-var timer = 0.0
+var jogador_detectado = false
+var bala_cena = preload("res://Scenes/bullet.tscn")
 
 func _physics_process(delta):
 	
-	if jogador == null:
-		return
-	timer -= delta
 	
 	var dir = jogador.global_position - global_position
 	var distancia = dir.length()
@@ -31,18 +30,30 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 		velocity.y = 0
-
-		if ray.is_colliding() and ray.get_collider().is_in_group("Jogador"):
-			if timer <= 0:
-				atirar()
-				timer = cooldown
-
+	
+	
 	move_and_slide()
 
-func atirar():
-	print("PEW PEW")
-	var bala = preload("res://Scripts/bullet.gd").instantiate()
-	get_tree().current_scene.add_child(bala)
-	bala.global_position = global_position = Vector3.UP
-	bala.direcao = (
-	player.global_position - bala.global_position).normalized()
+
+func _ready():
+	timer.timeout.connect(disparar)
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("Entrou algo:", body.name)
+	if body.is_in_group("Jogador"):
+		print("Jogador detectado")
+		jogador_detectado = true
+		timer.start()
+		disparar()
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+		if body.is_in_group("Jogador"):
+			jogador_detectado = false
+			timer.stop()
+
+func disparar():
+	var nova_bala = bala_cena.instantiate()
+	add_child(nova_bala)
+	nova_bala.global_position = global_position
+	nova_bala.look_at(jogador.global_position)
